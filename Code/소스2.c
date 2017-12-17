a3e8cd92870320f4c0c7bc36a7ddd8e1556d0e12
dldudwo <darldall@hanmail.net>
2017-12-17 09:56:25 +0900

    prac

---
+++ Code/소스2.c
@@ -0,0 +1,14 @@
+//#include<stdio.h>
+//int main(void)
+//{
+//	int dan = 2;
+//	int num = 1;
+//
+//	while (num <= 9)
+//	{
+//		printf("%d x %d = %d \n", dan, num*2-1, num*4-2);
+//		num++;
+//	}
+//
+//	return 0;
+//}
