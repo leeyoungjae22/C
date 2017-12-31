d3a614a2f3debdc917707abc59677301d0b83905
dldudwo <darldall@hanmail.net>
2017-12-31 10:49:52 +0900

    queue구조

---
+++ 새 폴더/dldudwo1.c
@@ -0,0 +1,10 @@
+﻿1>------ 빌드 시작: 프로젝트: ConsoleApplication37.c, 구성: Debug Win32 ------
+1>dldudwo.c
+1>c:\users\danawacomputer\documents\visual studio 2017\projects\consoleapplication37.c\consoleapplication37.c\dldudwo.c(24): warning C4996: 'scanf': This function or variable may be unsafe. Consider using scanf_s instead. To disable deprecation, use _CRT_SECURE_NO_WARNINGS. See online help for details.
+1>c:\program files (x86)\windows kits\10\include\10.0.14393.0\ucrt\stdio.h(1269): note: 'scanf' 선언을 참조하십시오.
+1>c:\users\danawacomputer\documents\visual studio 2017\projects\consoleapplication37.c\consoleapplication37.c\dldudwo.c(54): warning C4996: 'scanf': This function or variable may be unsafe. Consider using scanf_s instead. To disable deprecation, use _CRT_SECURE_NO_WARNINGS. See online help for details.
+1>c:\program files (x86)\windows kits\10\include\10.0.14393.0\ucrt\stdio.h(1269): note: 'scanf' 선언을 참조하십시오.
+1>ConsoleApplication37.c.vcxproj -> c:\users\danawacomputer\documents\visual studio 2017\Projects\ConsoleApplication37.c\Debug\ConsoleApplication37.c.exe
+1>ConsoleApplication37.c.vcxproj -> c:\users\danawacomputer\documents\visual studio 2017\Projects\ConsoleApplication37.c\Debug\ConsoleApplication37.c.pdb (Partial PDB)
+1>"ConsoleApplication37.c.vcxproj" 프로젝트를 빌드했습니다.
+========== 빌드: 성공 1, 실패 0, 최신 0, 생략 0 ==========
